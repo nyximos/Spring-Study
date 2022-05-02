cfdd14675fb894854fc067891ecd218ed04a35f5
nyximos <nyximos@gmail.com>
2022-05-03 00:26:06 +0900

    엔티티 클래스 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 연관관계의 거울, Order 테이블의 member필드에 의해 맵핑된거야 (읽기전용v)
+    private List<Order> orders = new ArrayList<>();
+}
