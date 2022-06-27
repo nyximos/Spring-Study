5bdfb8f7c2dde16cccc0ea332de19f38beb54218
nyximos <nyximos@gmail.com>
2022-06-27 14:42:29 +0900

    프로젝트 생성

---
+++ nyximoslog/src/main/java/com/nyximoslog/NyximoslogApplication.java
@@ -0,0 +1,13 @@
+package com.nyximoslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyximoslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyximoslogApplication.class, args);
+    }
+
+}
---
+++ nyximoslog/src/test/java/com/nyximoslog/NyximoslogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyximoslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyximoslogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
